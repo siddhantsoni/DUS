f2eb345e2ea0ee955552fcaa155141fea5d855ce
siddhantsoni <soni.siddhant@flipkart.com>
2020-01-07 13:05:00 +0530

    Purging UG before clearing Db

--- android/dus/src/main/java/com/flipkart/dus/DUSContentProvider.java
+++ android/dus/src/main/java/com/flipkart/dus/DUSContentProvider.java
@@ -286,10 +286,10 @@ public class DUSContentProvider extends ContentProvider {
                 purgeCacheAndDb();
                 break;
             case DUSContracts.CLEAR_UG:
-                purgeCacheAndDb();
                 if (getContext() != null) {
                     getScreenMaker().resetUpdateGraph(getContext(), true);
                 }
+                purgeCacheAndDb();
         }
         return 0;
     }
